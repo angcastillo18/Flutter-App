eae1f1e9fa2d903009469bbce75fa6e64a6bf664
angcastillo18 <angcastillo18@gmail.com>
2020-04-07 18:28:34 -0500

    added until lessons 16

--- ninjaIdProject/flutter_id/lib/main.dart
+++ ninjaIdProject/flutter_id/lib/main.dart
@@ -4,7 +4,16 @@ void main() => runApp(MaterialApp(
   home: NinjaCard(),
 ));
 
-class NinjaCard extends StatelessWidget {
+class NinjaCard extends StatefulWidget {
+  @override
+  _NinjaCardState createState() => _NinjaCardState();
+}
+
+class _NinjaCardState extends State<NinjaCard> {
+  //definir data dinamica
+  int ninjaLevel=0;
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,6 +23,17 @@ class NinjaCard extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.grey[850],
         elevation: 0.0,
+      ),
+      floatingActionButton: FloatingActionButton(
+          onPressed:(){
+            //ninjaLevel+=1; esto no funciona porque tenfria que vovler a construir la variable
+            setState(() {
+              ninjaLevel+=1;
+            });
+          },
+          child: Icon(Icons.add),
+          backgroundColor: Colors.grey[800],
+
       ),
       body:Padding(
         padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
@@ -57,7 +77,7 @@ class NinjaCard extends StatelessWidget {
             ),
             SizedBox(height: 10.0,),
             Text(
-              '8',
+              '$ninjaLevel',
               style: TextStyle(
                 color: Colors.amberAccent[200],
                 letterSpacing: 2.0,
@@ -89,3 +109,4 @@ class NinjaCard extends StatelessWidget {
     );
   }
 }
+
