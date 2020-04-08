0604bfbce3aea2e6a8d16229041779f8cd4ec050
angcastillo18 <angcastillo18@gmail.com>
2020-04-08 18:30:29 -0500

    added new project quotes

---
+++ quotesProject/quotes/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: QuoteList(),
+));
+
+class QuoteList extends StatefulWidget {
+  @override
+  _QuoteListState createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+  List<String> quotes=[
+    'Se tu mismo, todos ya lo hicieron',
+    'No tengo nada que declarar excepto mi sabiduria',
+    'El trato es raramente puro y simple'
+  ];
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text('Awesome Quotes'),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        children: quotes.map((quote)=>Text(quote)).toList(),
+      ),
+    );
+  }
+}
